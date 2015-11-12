b90f1e3ab5f85533d57026616935a40a1ea16c21
Daniel Fritz <dafritz@htwg-konstanz.de>
2015-11-12 16:27:25 +0100

    expanded ExpressionTest slightly to have two intermediate steps in one sub-expr

--- firstTryExpressions/src/main/ExpressionTest.java
+++ firstTryExpressions/src/main/ExpressionTest.java
@@ -14,7 +14,7 @@ public class ExpressionTest {
 
 
         // arithmetischer Ausdruck mit Klammerung:
-        Expr y = ExpressionBuilder.begin().expr(ExpressionBuilder.begin().expr(1).plus(2).end()).times(3).minus(ExpressionBuilder.begin().expr(4).divided(5).end()).end();
+        Expr y = ExpressionBuilder.begin().expr(ExpressionBuilder.begin().expr(1).plus(2).end()).times(3).minus(ExpressionBuilder.begin().expr(4).plus(1).divided(5).end()).end();
         System.out.printf("%s = %f%n", y, y.getValue());
 	}
 
