d99d0376b071c02cc51f94d4f8f5b72f51c9f74f
Daniel Fritz <dafritz@htwg-konstanz.de>
2015-12-16 12:56:25 +0100

    removed TODO

--- parseTree/src/parseTree/IMethod.java
+++ parseTree/src/parseTree/IMethod.java
@@ -4,7 +4,6 @@ import visitor.Visitable;
 
 public interface IMethod extends Visitable{	
 	public String toString(boolean isLastScope);
-	
-	// TODO getter (und setter) im Interface?
+
 	public String name();
 }
